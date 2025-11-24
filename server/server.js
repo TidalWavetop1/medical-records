355d83fb29bcfee75a9ad35c698e256e0d088b84
Kohaku Shiro <minhtoan13112005@gmail.com>
2025-11-24 15:26:54 +0700

    start project

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Backend running OK!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
